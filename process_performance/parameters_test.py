dccae240db7fdd7bf42f1155fe597deb5d313b35
Vladimir Looze <woldemar@mimas.ru>
2021-02-22 21:58:43 +0300

    Improved tests and coverage (#4)

--- process_performance/parameters_test.py
+++ process_performance/parameters_test.py
@@ -4,16 +4,15 @@
 """
 
 import pytest
-from process_performance.parameter import Parameter
 from process_performance.parameters import Parameters
 
 test_data = [
     (
-        [
-            Parameter(name="p1", values=[1, 2, 3]),
-            Parameter(name="p2", values=["a", "b"]),
-            Parameter(name="p3", values=["normal"]),
-        ],
+        {
+            "p1": [1, 2, 3],
+            "p2": ["a", "b"],
+            "p3": ["normal"],
+        },
         [
             ({'p1': 1}, {'p2': 'a'}, {'p3': 'normal'}),
             ({'p1': 1}, {'p2': 'b'}, {'p3': 'normal'}),
@@ -55,3 +54,9 @@ def test_generators(params, corners, cube, fill):
     assert list(param_space.gen_corners()) == corners
     assert list(param_space.gen_cube()) == cube
     assert list(param_space.gen_fill()) == fill
+
+
+def test_wrong_parameters():
+    """test_no_values"""
+    with pytest.raises(Parameters.WrongParametersType):
+        Parameters([])
